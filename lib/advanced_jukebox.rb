086c7bce4287bf48925971c3d39a690db00496ec
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-17 00:27:59 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,13 +3,13 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Go Go GO" => '/home/vlrathsmill-30063/jukebox-cli/audio/Emerald-Park/01.mp3',
+# "LiberTeens" => '/home/vlrathsmill-30063/jukebox-cli/audio/Emerald-Park/02.mp3',
+# "Hamburg" =>  '/home/vlrathsmill-30063/jukebox-cli/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => '/home/vlrathsmill-30063/jukebox-cli/audio/Emerald-Park/04.mp3',
+# "Wolf" => '</home/vlrathsmill-30063 /jukebox-cli/audio/Emerald-Park/05.mp3',
+# "Blue" => '</home/vlrathsmill-30063 /jukebox-cli/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => '/home/vlrathsmill-30063 /jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
 def help
@@ -20,7 +20,7 @@ end
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
 
@@ -33,7 +33,7 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,56 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "help : displays this help message"
+  puts "list : displays a list of songs you can play"
+  puts "play : lets you choose a song to play"
+  puts "exit : exits this program"
+end
+
+def list(songs)
+  songs. each_with_index do |song, index|
+    puts "#{index}. #{song}"
+  end
+end
+
+def play(songs)
+song = gets.chomp
+
+if songs.include?(song)
+  puts "Playing #{song}"
+else
+  song_num = song.to_i
+  if song_num < 1 || song_num > songs.length
+    puts "Invalid input, please try again"
+  else
+    puts "Playing #{songs[song_num - 1]}"
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+input = ""
+   while input
+    puts "Please enter a command:"
+     input = gets.strip
+     case input
+     when 'list'
+       list(songs)
+     when 'play'
+       list(songs)
+       play(songs)
+     when 'help'
+      help
+    when 'exit'
+           exit_jukebox
+      break
+   else
+      help
+     end
+  end
+end
